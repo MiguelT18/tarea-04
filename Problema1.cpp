8954279d084fe179e1672143d0b9361b48aa93ef
MiguelT18 <miguel.teranj02@gmail.com>
2026-09-16 23:19:47 -0400

    Creé un nuevo archivo Problema3.cpp y se modificó Problema1.cpp

--- Problema1.cpp
+++ Problema1.cpp
@@ -11,7 +11,7 @@ int main() {
 
   cin >> n;
 
-  cout << esMultiplo << endl;
+  cout << esMultiplo(n) << endl;
 
   return 0;
 }
---
+++ Problema3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int sumaMultiplosDeN(int a, int b, int n);
+
+int main() {
+    int a, b, n;
+    cin >> a >> b >> n;
+    cout << sumaMultiplosDeN(a, b, n) << endl;
+    
+    return 0;
+}
+
+int sumaMultiplosDeN(int a, int b, int n) {
+
+}
